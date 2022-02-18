e6a7a38096af2d611a1a948fd52033436f5b4385
michaelballos <michael124mike7@gmail.com>
2022-02-18 00:34:13 -0600

    Modify the button counter's count display element
    Modify so it is easier to display the user's current count. Switch it from a <p> element into a span so it is displayed through javascript easier. Button attributes were edited. Remove the class label on the buttons because it is redundent with a the clasification by Id. Background image file path  wrong and website not looking good.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-let x = document.getElementById('num');
-function count(x, y=1) {
-  return x+y
- }
+let x = 0;
+
+document.getElementById('num').innerHTML = x;
+
+function add() {
+	document.getElementById('num').innerHTML = ++x;
+}
 
 
+
+
+
+
+
+/*
+var x = 0;
+
+document.getElementById('output-area').innerHTML = x;
+
+function button1() {
+  document.getElementById('output-area').innerHTML = ++x;
+}
+
+function button2() {
+  document.getElementById('output-area').innerHTML = --x;
+}
+<input type=button value="increment" onclick="button1()" />
+<input type=button value="decrement" onclick="button2()" />
+<span id="output-area"></span>
+*/
